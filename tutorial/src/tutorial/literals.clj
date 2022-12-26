19c38a284ff9fa704fe1a59cab83b9c52065e046
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-26 22:37:07 +0900

    literals

---
+++ tutorial/src/tutorial/literals.clj
@@ -0,0 +1,19 @@
+42 ; integer
+-1.5 ; floating point
+22/7 ; ratio
+"hello" ; string
+\e ; character
+#"[0-9]+" ; regular expression
+
+map ; symbol
++ ; symbol
+clojure.core/+ ; namespaced symbol
+nil ; null value
+true false ; booleans
+:alpha ; keyword
+:release/alpha ; keyword with namespace
+
+'(1 2 3) ; list
+[1 2 3] ; vector
+#{1 2 3} ; set
+{:a 1, :b 2} ; map
