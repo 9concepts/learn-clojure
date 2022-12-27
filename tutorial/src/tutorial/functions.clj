8ec992c097f95d51a4ddb62ff6c1ed2ee4fc192b
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-27 21:52:17 +0900

    Playging functions

--- tutorial/src/tutorial/functions.clj
+++ tutorial/src/tutorial/functions.clj
@@ -40,3 +40,29 @@
 ; variadic-function
 (defn hello [greeting & who]
   (println greeting who))
+
+((fn [message] (println message)) "hello")
+
+(defn greet_men [name] (str "Hello, " name))
+(def greet_men (fn [name] (str "Hello, " name)))
+
+#(+ 6 %)
+
+#(+ %1 %2)
+
+#(println %1 %2 %&)
+
+(defn greet [] println "Hello")
+
+(def greet (fn [] (println "Hello")))
+
+(def greet #(println "Hello"))
+
+(defn greeting
+  ([] "Hello, World!")
+  ([x] (str "Hello, " x "!"))
+  ([x y] (str x ", " y "!")))
+
+(assert (= "Hello, World!" (greeting)))
+(assert (= "Hello, Clojure!" (greeting "Clojure")))
+(assert (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))
